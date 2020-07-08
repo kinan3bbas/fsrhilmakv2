3dbd79a6724b1d66ec77f9aa47d709ccd621bbad
kinan Abbas <kinan.magnamedia@gmail.com>
2020-07-08 21:48:52 +0300

    changes

---
+++ fsrhilmakv2/Models/ServicePath.cs
@@ -0,0 +1,37 @@
+﻿using fsrhilmakv2.Extras;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace fsrhilmakv2.Models
+{
+    public class ServicePath:BasicModel
+    {
+        [Required]
+        [Display(Name = "Name")]
+        public String Name { get; set; }
+
+        [Required]
+        [Display(Name = "Cost")]
+        public double Cost { get; set; }
+
+        [Required]
+        [Display(Name = "Enabled")]
+        public bool Enabled { get; set; }
+
+        [Required]
+        [Display(Name = "Is Private")]
+        public bool IsPrivate { get; set; }
+
+        public String ServiceProviderId { get; set; }
+
+        public ApplicationUser ServiceProvider { get; set; }
+
+
+
+
+
+    }
+}
