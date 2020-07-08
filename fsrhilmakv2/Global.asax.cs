29eb98fa8583c2f53c692cda19fb7289e457e1eb
kinan Abbas <kinan.magnamedia@gmail.com>
2020-07-08 21:46:03 +0200

    translate comm

--- fsrhilmakv2/Global.asax.cs
+++ fsrhilmakv2/Global.asax.cs
@@ -1,7 +1,9 @@
 ﻿using fsrhilmakv2.App_Start;
 using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
+using System.Threading;
 using System.Web;
 using System.Web.Http;
 using System.Web.Mvc;
@@ -21,6 +23,17 @@ namespace fsrhilmakv2
             BundleConfig.RegisterBundles(BundleTable.Bundles);
            // FormatterConfig.RegisterFormatters(GlobalConfiguration.Configuration.Formatters);
 
+        }
+
+        protected void Application_AcquireRequestState(object sender, EventArgs e)
+        {
+            string culture = "en-US";
+            if (Request.UserLanguages != null)
+            {
+                culture = Request.UserLanguages[0];
+            }
+            Thread.CurrentThread.CurrentCulture = CultureInfo.GetCultureInfo(culture);
+            Thread.CurrentThread.CurrentUICulture = CultureInfo.GetCultureInfo(culture);
         }
     }
 }
